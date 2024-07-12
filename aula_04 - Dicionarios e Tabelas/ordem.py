4070010de844df44ef7a07ca570d3e6c26be9d0f
unknown <gcbertolaccini2@gmail.com>
2024-07-12 15:27:18 -0300

    Ajustando ordem.py

--- aula_04 - Dicionarios e Tabelas/ordem.py
+++ aula_04 - Dicionarios e Tabelas/ordem.py
@@ -1,13 +1,10 @@
+# Implementação do algoritmo de ordenação por seleção
 lista = [64, 34, 25, 12, 22, 11, 90]
 
-def ordernar_lista(lista: list) -> list:
-    lista_ordenada = lista.copy()
+for i in range(len(lista)):
+    for j in range(i+1, len(lista)):
+        if lista[i] > lista[j]:
+            lista[i], lista[j] = lista[j], lista[i]
 
-    for i in range(len(lista)):
-        for j in range(i+1, len(lista)):
-            if lista[i] > lista[j]:
-                lista[i], lista[j] = lista[j], lista[i]
-
-    return lilista_ordenadasta
-
-print(lista)
+# Ordenando a lista
+print("Lista ordenada com função personalizada:", lista)
